d409ff41bbe11cbc7fb05907095570134429f8ef
Vojta Jina <vojta.jina@gmail.com>
2014-04-03 16:56:31 -0700

    refactor(testing): use providers

--- src/testing.js
+++ src/testing.js
@@ -1,6 +1,8 @@
 import {Injector} from './injector';
 import {Inject, annotate, readAnnotations} from './annotations';
-import {isUpperCase, isClass, isFunction, isObject} from './util';
+import {isFunction} from './util';
+import {createProviderFromFnOrClass} from './providers';
+
 
 var currentSpec = null;
 beforeEach(function() {
@@ -69,44 +71,13 @@ function inject(...params) {
         } else {
           if (!isFunction(providerWrapper.provider)) {
             // inlined mock
-            // TODO(vojta): use FactoryProvider
-            providers.set(providerWrapper.as, {
-              /*jshint loopfunc:true */
-              provider: function() {return providerWrapper.provider;},
-              /*jshint loopfunc:false */
-              isPromise: false,
-              params: [],
-              paramsPromises: [],
-              isClass: false,
-              /*jshint loopfunc:true */
-              create: function() {return providerWrapper.provider;}
-              /*jshint loopfunc:false */
-            });
+            providers.set(providerWrapper.as, createProviderFromFnOrClass(function() {
+              return providerWrapper.provider;
+            }, {isPromise: false, injectTokens: [], injectPromises: [], injectLazily: []}));
           } else {
             // a fn/class provider with overriden token
-            // TODO(vojta): use Class/FactoryProvider
             annotations = readAnnotations(providerWrapper.provider);
-            providers.set(providerWrapper.as, {
-              provider: providerWrapper.provider,
-              isPromise: annotations.isPromise,
-              params: annotations.injectTokens,
-              paramsPromises: annotations.injectPromises,
-              isClass: isClass(providerWrapper.provider),
-              create: function(args) {
-                if (isClass(providerWrapper.provider)) {
-                  var context = Object.create(providerWrapper.provider.prototype);
-                  var returnedValue = providerWrapper.provider.apply(context, args);
-
-                  if (!isFunction(returnedValue) && !isObject(returnedValue)) {
-                    return context;
-                  }
-
-                  return returnedValue;
-                } else {
-                  return providerWrapper.provider.apply(undefined, args);
-                }
-              }
-            });
+            providers.set(providerWrapper.as, createProviderFromFnOrClass(providerWrapper.provider, annotations));
           }
         }
       });
